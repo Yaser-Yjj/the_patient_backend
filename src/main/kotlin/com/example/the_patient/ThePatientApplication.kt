f506ddc2f2311b93c28393180b33d379e554cfea
Yaser-Yjj <yaseryjjou@gmail.com>
2025-07-09 17:02:12 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/the_patient/ThePatientApplication.kt
@@ -0,0 +1,11 @@
+package com.example.the_patient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThePatientApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThePatientApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/the_patient/ThePatientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.the_patient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThePatientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
